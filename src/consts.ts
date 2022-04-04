54c91518e6ec1d8b8ab0e72e7c02072fc5259717
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2022-04-04 14:25:03 +0300

    ...

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const REACT_APP_API_URL: string = 'http://vps.razumeetsya.ru:5000/'
 // export const REACT_APP_API_URL: string = 'http://192.168.1.107:5000/'
-export const REACT_APP_URL: string = 'http://localhost:3001/'
+export const REACT_APP_URL: string = 'http://vps.razumeetsya.ru:3000/'
 
 export enum routes{
     ADMIN_ROUTE = '/admin',
